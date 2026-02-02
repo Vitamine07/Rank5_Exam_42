22fe1b2885bb078308bcf66bb47bac041c958b28
vitamine07 <amine.b67@hotmail.com>
2026-02-02 16:17:37 +0100

    fini

---
+++ vect2.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+
+class vect2
+{
+private:
+	int x;
+	int y;
+public:
+	vect2();
+	vect2(int a, int b);
+	vect2(const vect2& other);
+	vect2 &operator=(const vect2& other);
+	~vect2();
+	
+	int& operator[](int num);
+	const int& operator[](int num) const;
+	vect2 operator+(const vect2&other)const;
+	vect2 operator-(const vect2&other)const;
+	vect2 operator-();
+	vect2 operator*(const vect2&other)const;
+	vect2 operator*(int n)const;
+	vect2 &operator+=(const vect2&other);
+	vect2 &operator-=(const vect2&other);
+	vect2 &operator*=(const vect2&other);
+	
+
+
+
+
+	
+
+
+};
