f2e7e50e3d6af703049307a1c21d9ed2476db80e
vitamine07 <amine.b67@hotmail.com>
2026-02-03 16:08:53 +0100

    apprendre vect2

--- level0/vect2/vect2.hpp
+++ level0/vect2/vect2.hpp
@@ -25,6 +25,13 @@ class vect2
 		vect2&	operator-=(const vect2& other);
 		vect2	operator*(const vect2& other)const;
 		vect2&	operator*=(const vect2& other);
+
+
+
+
+
+
+		
 		vect2&	operator++();
 		vect2	operator++(int);
 		vect2&	operator--();
--- vect2.hpp
+++ vect2.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>
-
 class vect2
 {
 private:
@@ -10,12 +8,12 @@ private:
 public:
 	vect2();
 	vect2(int a, int b);
-	vect2(const vect2& other);
-	vect2 &operator=(const vect2& other);
+	vect2 (const vect2& other);
+	vect2 &operator=(const vect2 other);
 	~vect2();
 	
 	int& operator[](int num);
-	const int& operator[](int num) const;
+	const int& operator[](int num)const;
 	vect2 operator+(const vect2&other)const;
 	vect2 operator-(const vect2&other)const;
 	vect2 operator-();
@@ -24,12 +22,15 @@ public:
 	vect2 &operator+=(const vect2&other);
 	vect2 &operator-=(const vect2&other);
 	vect2 &operator*=(const vect2&other);
-	
 
+	vect2& operator++();
+	vect2 operator++(int);
+	vect2& operator--();
+	vect2 operato(int);
 
-
-
-	
-
-
+	bool operator==(const vect2& other)const;
+	bool operator!=(const vect2& other)const;
 };
+
+vect2 operator*(int n, const vect2&other);
+std::ostream& operator<<(std::ostream &os, const vect2& obj);
